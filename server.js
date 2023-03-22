8fe5d07fe5e53ce28fe1d433b564b769e2398f33
udbhav3101 <udbhav3101@gmail.com>
2023-03-22 13:03:24 +0530

    changed server

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const jsonServer = require('json-server');
-const express = require('express');
-const path = require('path');
 const server = jsonServer.create();
 
 const router = jsonServer.router('./db.json');
 
-const middlewares = jsonServer.defaults({
-  static: './build',
-});
+const middlewares = jsonServer.defaults();
 
 const PORT = process.env.PORT || 8000;
 
@@ -20,10 +16,7 @@ server.use(
 );
 
 server.use(router);
-server.use(express.static(path.join(__dirname, 'build')));
-server.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+
 server.listen(PORT, () => {
   console.log('Server is running');
 });
